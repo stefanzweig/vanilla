1b3af9d1a6f7ef63cf10fd1b1457b2871cb5d4f6
Stefan Zweig <stefan.zweig@gmail.com>
2020-10-08 12:07:45 +0800

    font in mac

--- personal/fonts.el
+++ personal/fonts.el
@@ -1,4 +1,4 @@
 ;; font
 (if (eq system-type 'darwin)
-  (set-face-attribute 'default nil :family "Monaco" :height 100 :weight 'normal)
+  (set-face-attribute 'default nil :family "Source Code Pro" :height 160 :weight 'normal)
   (set-face-attribute 'default nil :family "Source Code Pro" :height 100 :weight 'normal))
