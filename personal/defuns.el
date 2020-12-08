bc6381425e657f39e55a254783f81b565cdc2902
刘海江 <haijiang.liu@wz-inc.com>
2020-12-08 14:54:26 +0800

    better font

--- personal/defuns.el
+++ personal/defuns.el
@@ -24,3 +24,21 @@
 	    (autoload autoload-function (symbol-name library) nil t))
 	(require library)))
     (personal library)))
+
+(defun +my/better-font()
+  (interactive)
+  ;; english font
+  (if (display-graphic-p)
+      (progn
+	(set-face-attribute 'default nil :font (format   "%s:pixelsize=%d" "Sarasa Mono SC Nerd" 16)) ;; 11 13 17 19 23
+	;; chinese font
+	(dolist (charset '(kana han symbol cjk-misc bopomofo))
+	  (set-fontset-font (frame-parameter nil 'font)
+			    charset
+			    (font-spec :family "Sarasa Mono SC Nerd")))) ;; 14 16 20 22 28
+    ))
+
+(defun +my|init-font(frame)
+  (with-selected-frame frame
+    (if (display-graphic-p)
+	(+my/better-font))))
