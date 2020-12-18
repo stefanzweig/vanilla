633c81ad31a761a4246aa9f2cdf40c5b2e1d26cb
刘海江 <haijiang.liu@wz-inc.com>
2020-12-18 09:37:02 +0800

    current file's todo list.

--- personal/defuns.el
+++ personal/defuns.el
@@ -42,3 +42,12 @@
   (with-selected-frame frame
     (if (display-graphic-p)
 	(+my/better-font))))
+
+
+(defun org-todo-list-current-file (&optional arg)
+  "Like `org-todo-list', but using only the current buffer's file."
+  (interactive "P")
+  (let ((org-agenda-files (list (buffer-file-name (current-buffer)))))
+    (if (null (car org-agenda-files))
+	(error "%s is not visiting a file" (buffer-name (current-buffer)))
+      (org-todo-list arg))))
