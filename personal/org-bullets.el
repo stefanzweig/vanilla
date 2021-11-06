282d7f818cdd4e334f891be539c6185c016c1cd0
Stefan Zweig <stefan.zweig@gmail.com>
2021-11-07 07:55:32 +0800

    cl-lib

--- personal/org-bullets.el
+++ personal/org-bullets.el
@@ -27,7 +27,7 @@
 
 ;;; Code:
 
-(eval-when-compile (require 'cl))
+(eval-when-compile (require 'cl-lib))
 
 (defgroup org-bullets nil
   "Display bullets as UTF-8 characters"
@@ -62,9 +62,9 @@ It can contain any number of symbols, which will be repeated."
     (mouse-1 . org-cycle)
     (mouse-2
      . (lambda (e)
-         (interactive "e")
-         (mouse-set-point e)
-         (org-cycle))))
+	 (interactive "e")
+	 (mouse-set-point e)
+	 (org-cycle))))
   "Mouse events for bullets.
 Should this be undesirable, one can remove them with
 \(setcdr org-bullets-bullet-map nil\)")
@@ -72,52 +72,52 @@ Should this be undesirable, one can remove them with
 (defun org-bullets-level-char (level)
   (string-to-char
    (nth (mod (1- level)
-             (length org-bullets-bullet-list))
-        org-bullets-bullet-list)))
+	     (length org-bullets-bullet-list))
+	org-bullets-bullet-list)))
 
 ;;;###autoload
 (define-minor-mode org-bullets-mode
     "UTF8 Bullets for org-mode"
   nil nil nil
   (let* (( keyword
-           `(("^\\*+ "
-              (0 (let* (( level (- (match-end 0) (match-beginning 0) 1))
-                        ( is-inline-task
-                          (and (boundp 'org-inlinetask-min-level)
-                               (>= level org-inlinetask-min-level))))
-                   (compose-region (- (match-end 0) 2)
-                                   (- (match-end 0) 1)
-                                   (org-bullets-level-char level))
-                   (when is-inline-task
-                     (compose-region (- (match-end 0) 3)
-                                     (- (match-end 0) 2)
-                                     (org-bullets-level-char level)))
-                   (when (facep org-bullets-face-name)
-                     (put-text-property (- (match-end 0)
-                                           (if is-inline-task 3 2))
-                                        (- (match-end 0) 1)
-                                        'face
-                                        org-bullets-face-name))
-                   (put-text-property (match-beginning 0)
-                                      (- (match-end 0) 2)
-                                      'face (list :foreground
-                                                  (face-attribute
-                                                   'default :background)))
-                   (put-text-property (match-beginning 0)
-                                      (match-end 0)
-                                      'keymap
-                                      org-bullets-bullet-map)
-                   nil))))))
+	   `(("^\\*+ "
+	      (0 (let* (( level (- (match-end 0) (match-beginning 0) 1))
+			( is-inline-task
+			  (and (boundp 'org-inlinetask-min-level)
+			       (>= level org-inlinetask-min-level))))
+		   (compose-region (- (match-end 0) 2)
+				   (- (match-end 0) 1)
+				   (org-bullets-level-char level))
+		   (when is-inline-task
+		     (compose-region (- (match-end 0) 3)
+				     (- (match-end 0) 2)
+				     (org-bullets-level-char level)))
+		   (when (facep org-bullets-face-name)
+		     (put-text-property (- (match-end 0)
+					   (if is-inline-task 3 2))
+					(- (match-end 0) 1)
+					'face
+					org-bullets-face-name))
+		   (put-text-property (match-beginning 0)
+				      (- (match-end 0) 2)
+				      'face (list :foreground
+						  (face-attribute
+						   'default :background)))
+		   (put-text-property (match-beginning 0)
+				      (match-end 0)
+				      'keymap
+				      org-bullets-bullet-map)
+		   nil))))))
     (if org-bullets-mode
-        (progn
-          (font-lock-add-keywords nil keyword)
-          (font-lock-fontify-buffer))
+	(progn
+	  (font-lock-add-keywords nil keyword)
+	  (font-lock-fontify-buffer))
       (save-excursion
-        (goto-char (point-min))
-        (font-lock-remove-keywords nil keyword)
-        (while (re-search-forward "^\\*+ " nil t)
-          (decompose-region (match-beginning 0) (match-end 0)))
-        (font-lock-fontify-buffer))
+	(goto-char (point-min))
+	(font-lock-remove-keywords nil keyword)
+	(while (re-search-forward "^\\*+ " nil t)
+	  (decompose-region (match-beginning 0) (match-end 0)))
+	(font-lock-fontify-buffer))
       )))
 
 (provide 'org-bullets)
