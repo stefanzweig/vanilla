ae2be972cf2e10d4584da8459c649bd6a071b242
Stefan Zweig <stefan.zweig@gmail.com>
2022-05-08 17:59:40 +0800

    center the frame by default

--- personal/zweig.el
+++ personal/zweig.el
@@ -4,5 +4,27 @@
     (interactive)
   (insert (now)))
 
+
+(defun my/frame-recenter (&optional frame)
+  "Center FRAME on the screen.
+FRAME can be a frame name, a terminal name, or a frame.
+If FRAME is omitted or nil, use currently selected frame."
+  (interactive)
+  (unless (eq 'maximised (frame-parameter nil 'fullscreen))
+    (let* ((frame (or (and (boundp 'frame)
+			    frame)
+		      (selected-frame)))
+	   (frame-w (frame-pixel-width frame))
+	   (frame-h (frame-pixel-height frame))
+	   ;; frame-monitor-workarea returns (x y width height) for the monitor
+	   (monitor-w (nth 2 (frame-monitor-workarea frame)))
+	   (monitor-h (nth 3 (frame-monitor-workarea frame)))
+	   (center (list (/ (- monitor-w frame-w) 2)
+			 (/ (- monitor-h frame-h) 2))))
+      (apply 'set-frame-position (flatten-list (list frame center))))))
+
+(add-hook 'after-init-hook #'my/frame-recenter)
+(add-hook 'after-make-frame-functions #'my/frame-recenter)
+
 (provide 'zweig)
 ;;; zweig.el ends here
